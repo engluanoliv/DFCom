6961c95c449a2ccf6e8397d38e389023b822facd
engluanoliv <engluanoliv@gmail.com>
2025-09-18 16:25:05 -0300

    created the methods of the product

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createProduct } from "../controllers/productController";
+import {
+  createProduct,
+  deleteProduct,
+  getProduct,
+  updateProduct,
+} from "../controllers/productController";
 
 const router = express.Router();
 
 router.post("/", createProduct);
-
 router.get("/", listProducts);
+router.get("/:id", getProduct);
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
