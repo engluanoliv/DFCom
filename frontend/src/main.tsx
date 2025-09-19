90fa8f875d71af5157ae4c3ec0d715c2b34a901d
engluanoliv <engluanoliv@gmail.com>
2025-09-19 00:59:46 -0300

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,8 +7,8 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
+    {/* <ThemeProvider> */}
       <App />
-    </ThemeProvider>
+    {/* </ThemeProvider> */}
   </StrictMode>
 );
